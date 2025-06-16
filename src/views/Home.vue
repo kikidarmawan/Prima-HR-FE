7a1aff367a1fdeaaa2dcc6764c636785c685134e
Kiki Darmawan <kikiedarmawan@gmail.com>
2025-06-16 11:01:47 +0700

    Setup Project

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>Count: {{ count }}</p>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+    computed: {
+        ...mapState(['count'])
+    },
+    methods: {
+        ...mapMutations(['increment'])
+    }
+}
+</script>
+<style scoped></style>
