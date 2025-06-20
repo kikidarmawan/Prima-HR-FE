b59be66974b66a54bfa25ac5d0684bdb7b206c7a
Ryznto <riyanto112211@gmail.com>
2025-06-20 16:45:33 +0700

    merapihkan

--- src/pages/leave/ApplyLeave.vue
+++ src/pages/leave/ApplyLeave.vue
@@ -45,24 +45,29 @@ const onNumberInput = (e) => {
         />
       </div>
 
-      <!-- Leave Type -->
-      <div class="border border-blue-500 rounded-lg p-2">
-        <label class="text-xs text-blue-500">Leave Type</label>
-        <div class="relative">
-          <select class="w-full appearance-none outline-none pr-8">
-            <option>Medical Leave</option>
-            <option>Annual Leave</option>
-            <option>Emergency Leave</option>
-          </select>
-          <i
-            class="fa-solid fa-chevron-down absolute right-2 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none"
-          ></i>
-        </div>
+      <div class="relative w-full ">
+        <select
+          id="leaveType"
+          class="peer w-full appearance-none border border-blue-500 text-sm text-gray-700 bg-white rounded-lg pt-5 pb-2 pl-3 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500  ">
+          <option value="">Medical Leave</option>
+          <option>Annual Leave</option>
+          <option>Emergency Leave</option>
+        </select>
+
+        <!-- Floating label -->
+        <label
+          for="leaveType"
+          class="absolute space-y-5 left-3 top-2 text-xs text-blue-500 bg-white px-1 transition-all peer-focus:top-1 peer-focus:text-[10px] peer-focus:text-blue-600 pointer-events-none">
+          Leave Type
+        </label>
+        <!-- Chevron icon -->
+        <i
+          class="fa-solid fa-chevron-down absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none"></i>
       </div>
 
       <!-- Contact Number -->
       <div class="border border-blue-500 rounded-lg p-2 w-full">
-        <label class="text-xs text-blue-500">Contact Number</label>
+        <label class="text-xs text-blue-500 ">Contact Number</label>
         <input
           type="text"
           placeholder="(+62)"
@@ -72,19 +77,24 @@ const onNumberInput = (e) => {
         />
       </div>
 
-      <!-- Start Date -->
-      <div class="border border-blue-500 rounded-lg p-2">
-        <label class="text-xs text-blue-500">Start Date</label>
-        <div>
-          <input type="date" class="w-full outline-none pr-8" />
+      <div class="space-y-4">
+        <!-- Start Date -->
+        <!-- Start Date -->
+        <div class="border border-blue-500 rounded-lg p-2">
+          <label class="text-xs text-blue-500">Start Date</label>
+          <input
+            type="date"
+            class="w-full outline-none pr-2 text-sm text-gray-700"
+          />
         </div>
-      </div>
 
-      <!-- End Date -->
-      <div class="border border-blue-500 rounded-lg p-2">
-        <label class="text-xs text-blue-500">End Date</label>
-        <div class="relative">
-          <input type="date" class="w-full outline-none pr-8 appearance-none" />
+        <!-- End Date -->
+        <div class="border border-blue-500 rounded-lg p-2">
+          <label class="text-xs text-blue-500">End Date</label>
+          <input
+            type="date"
+            class="w-full outline-none pr-2 text-sm text-gray-700"
+          />
         </div>
       </div>
 
@@ -101,7 +111,8 @@ const onNumberInput = (e) => {
       <!-- Button apply -->
       <button
         type="submit"
-        class="w-full mt-5 bg-blue-500 text-white cursor-pointer font-semibold py-3 rounded-xl hover:bg-blue-600 transition">
+        class="w-full mt-5 bg-blue-500 text-white cursor-pointer font-semibold py-3 rounded-xl hover:bg-blue-600 transition"
+      >
         Apply Leave
       </button>
     </form>
