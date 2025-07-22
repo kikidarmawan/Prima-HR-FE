b5ea84a786fd49f37030f1a00f19253b4bda3dc9
Ryznto <riyanto112211@gmail.com>
2025-07-22 13:12:25 +0700

    holiday with api

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -52,22 +52,32 @@ const maxDrag = 200;
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-50 dark:bg-black dark:text-white overflow-y-auto pb-28 transition-colors duration-300">
+  <div
+    class="min-h-screen bg-gray-50 dark:bg-black dark:text-white overflow-y-auto pb-28 transition-colors duration-300"
+  >
     <!-- Header -->
     <div class="flex justify-between items-center p-5 gap-5 w-full">
       <div class="flex items-center gap-5">
         <img :src="Oren" class="rounded-full w-[52px] h-[52px] object-cover" />
         <div>
-          <h1 class="text-xl font-semibold text-gray-900 dark:text-white">Michael Mitc</h1>
-          <p class="text-black text-xs dark:text-gray-300">Lead UI/UX Designer</p>
+          <h1 class="text-xl font-semibold text-gray-900 dark:text-white">
+            Michael Mitc
+          </h1>
+          <p class="text-black text-xs dark:text-gray-300">
+            Lead UI/UX Designer
+          </p>
         </div>
       </div>
 
       <!-- Bell Icon -->
       <div class="ml-auto">
         <router-link to="/notif">
-          <div class="w-12 h-12 flex items-center justify-center rounded-full border border-gray-300 dark:border-gray-600 shadow-sm bg-white dark:bg-gray-800">
-            <i class="fa-regular fa-bell text-lg text-gray-700 dark:text-gray-300"></i>
+          <div
+            class="w-12 h-12 flex items-center justify-center rounded-full border border-gray-300 dark:border-gray-600 shadow-sm bg-white dark:bg-gray-800"
+          >
+            <i
+              class="fa-regular fa-bell text-lg text-gray-700 dark:text-gray-300"
+            ></i>
           </div>
         </router-link>
       </div>
@@ -98,23 +108,35 @@ const maxDrag = 200;
         <div class="flex gap-4">
           <div class="bg-white dark:bg-gray-800 w-40 p-4 rounded-2xl shadow-sm">
             <div class="flex items-center gap-2 mb-2">
-              <div class="bg-blue-100 dark:bg-blue-900 w-8 h-8 flex items-center justify-center rounded-md">
+              <div
+                class="bg-blue-100 dark:bg-blue-900 w-8 h-8 flex items-center justify-center rounded-md"
+              >
                 <i class="fa-solid fa-arrow-right text-[#4893fc]"></i>
               </div>
-              <h2 class="text-sm font-medium text-gray-700 dark:text-gray-300">Check In</h2>
+              <h2 class="text-sm font-medium text-gray-700 dark:text-gray-300">
+                Check In
+              </h2>
             </div>
-            <h1 class="text-xl font-bold text-gray-900 dark:text-white">10:20 <span class="lowercase">am</span></h1>
+            <h1 class="text-xl font-bold text-gray-900 dark:text-white">
+              10:20 <span class="lowercase">am</span>
+            </h1>
             <p class="text-sm text-gray-500 dark:text-gray-400">On Time</p>
           </div>
 
           <div class="bg-white dark:bg-gray-800 w-40 p-4 rounded-2xl shadow-sm">
             <div class="flex items-center gap-2 mb-2">
-              <div class="bg-blue-100 dark:bg-blue-900 w-8 h-8 flex items-center justify-center rounded-md">
+              <div
+                class="bg-blue-100 dark:bg-blue-900 w-8 h-8 flex items-center justify-center rounded-md"
+              >
                 <i class="fa-solid fa-arrow-left text-[#4893fc]"></i>
               </div>
-              <h2 class="text-sm font-medium text-gray-700 dark:text-gray-300">Check Out</h2>
+              <h2 class="text-sm font-medium text-gray-700 dark:text-gray-300">
+                Check Out
+              </h2>
             </div>
-            <h1 class="text-xl font-bold text-gray-900 dark:text-white">07:00 <span class="lowercase">pm</span></h1>
+            <h1 class="text-xl font-bold text-gray-900 dark:text-white">
+              07:00 <span class="lowercase">pm</span>
+            </h1>
             <p class="text-sm text-gray-500 dark:text-gray-400">Go Home</p>
           </div>
         </div>
@@ -128,34 +150,54 @@ const maxDrag = 200;
       </h1>
 
       <div class="space-y-3 mt-3">
-        <div class="bg-white dark:bg-gray-800 w-full p-4 rounded-2xl shadow-sm flex justify-between items-center">
+        <div
+          class="bg-white dark:bg-gray-800 w-full p-4 rounded-2xl shadow-sm flex justify-between items-center"
+        >
           <div class="flex items-center gap-3">
-            <div class="bg-blue-100 dark:bg-blue-900 w-8 h-8 flex items-center justify-center rounded-md">
+            <div
+              class="bg-blue-100 dark:bg-blue-900 w-8 h-8 flex items-center justify-center rounded-md"
+            >
               <i class="fa-solid text-sm fa-arrow-right text-[#4893fc]"></i>
             </div>
             <div>
-              <h2 class="text-sm font-medium text-gray-800 dark:text-white">Check In</h2>
-              <p class="text-xs text-gray-400 dark:text-gray-300">April 17, 2023</p>
+              <h2 class="text-sm font-medium text-gray-800 dark:text-white">
+                Check In
+              </h2>
+              <p class="text-xs text-gray-400 dark:text-gray-300">
+                April 17, 2023
+              </p>
             </div>
           </div>
           <div class="text-right">
-            <h1 class="text-base font-bold text-gray-900 dark:text-white">10:00 <span class="lowercase">am</span></h1>
+            <h1 class="text-base font-bold text-gray-900 dark:text-white">
+              10:00 <span class="lowercase">am</span>
+            </h1>
             <p class="text-sm text-gray-500 dark:text-gray-400">On Time</p>
           </div>
         </div>
 
-        <div class="bg-white dark:bg-gray-800 w-full p-4 rounded-2xl shadow-sm flex justify-between items-center">
+        <div
+          class="bg-white dark:bg-gray-800 w-full p-4 rounded-2xl shadow-sm flex justify-between items-center"
+        >
           <div class="flex items-center gap-3">
-            <div class="bg-blue-100 dark:bg-blue-900 w-8 h-8 flex items-center justify-center rounded-md">
+            <div
+              class="bg-blue-100 dark:bg-blue-900 w-8 h-8 flex items-center justify-center rounded-md"
+            >
               <i class="fa-solid text-xs fa-mug-saucer text-[#4893fc]"></i>
             </div>
             <div>
-              <h2 class="text-sm font-medium text-gray-800 dark:text-white">Break In</h2>
-              <p class="text-xs text-gray-400 dark:text-gray-300">April 17, 2023</p>
+              <h2 class="text-sm font-medium text-gray-800 dark:text-white">
+                Break In
+              </h2>
+              <p class="text-xs text-gray-400 dark:text-gray-300">
+                April 17, 2023
+              </p>
             </div>
           </div>
           <div class="text-right">
-            <h1 class="text-base font-bold text-gray-900 dark:text-white">12:30 <span class="lowercase">pm</span></h1>
+            <h1 class="text-base font-bold text-gray-900 dark:text-white">
+              12:30 <span class="lowercase">pm</span>
+            </h1>
             <p class="text-sm text-gray-500 dark:text-gray-400">On Time</p>
           </div>
         </div>
@@ -165,9 +207,11 @@ const maxDrag = 200;
       <div class="mt-6 flex justify-center">
         <div
           class="w-full max-w-md rounded-xl relative px-5 py-3 shadow-md overflow-hidden select-none transition-colors"
-          :class="isCheckedIn
+          :class="
+            isCheckedIn
               ? 'bg-gradient-to-r from-pink-400 to-rose-400'
-              : 'bg-gradient-to-r from-blue-500 to-[#4893fc]'"
+              : 'bg-gradient-to-r from-blue-500 to-[#4893fc]'
+          "
           @pointerdown="startDrag"
           @pointermove="onDrag"
           @pointerup="endDrag"
