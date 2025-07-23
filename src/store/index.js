40436a88998392cfb710720b5aa0c43e088e91a5
Ryznto <riyanto112211@gmail.com>
2025-07-23 10:46:52 +0700

    hoiliday with api fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 import auth from './modules/auth'
+import holiday from "./modules/holiday";
 
 export default createStore({
   modules: {
-    auth
+    auth,
+    holiday,
   }
 })
---
+++ src/store/modules/holiday.js
@@ -0,0 +1,30 @@
+import api from "@/services/api";
+
+export default {
+  namespaced: true,
+  state: () => ({
+    holidays: [],
+  }),
+  mutations: {
+    setHolidays(state, holidays) {
+      state.holidays = holidays;
+      console.log(holidays);
+    },
+  },
+  actions: {
+    async fetchHolidays({ commit }) {
+      try {
+        const response = await api.get("/api/holiday");
+        const data = response.data.data || [];
+        const updated = data.map((item) => ({
+          //updated
+          ...item,
+          highlight: true,
+        }));
+        commit("setHolidays", updated);
+      } catch (error) {
+        console.error("Error fetching holidays from API:", error);
+      }
+    },
+  },
+};
