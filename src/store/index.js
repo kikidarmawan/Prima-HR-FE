ab48ce45a845813c9017c091c02494e0085f700a
Ryznto <riyanto112211@gmail.com>
2025-07-29 09:45:21 +0700

    perubahan

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,13 @@
-import { createStore } from 'vuex'
-import auth from './modules/auth'
-import holiday from './modules/holiday'
-import karyawan from './modules/karyawan'
 import { createStore } from "vuex";
 import auth from "./modules/auth";
 import holiday from "./modules/holiday";
-import detail_karyawan from "./modules/detail_karyawan";
+import karyawan from "./modules/karyawan";
+
 
 export default createStore({
   modules: {
     auth,
     holiday,
     karyawan,
-  }
-})
-    detail_karyawan,
   },
 });
