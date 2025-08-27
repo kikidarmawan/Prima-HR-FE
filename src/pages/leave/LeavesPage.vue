572c7dcc8cf36199547664461ea9906326cc7a0c
Ryznto <riyanto112211@gmail.com>
2025-08-27 13:07:16 +0700

    menambahkan loading

--- src/pages/leave/LeavesPage.vue
+++ src/pages/leave/LeavesPage.vue
@@ -155,14 +155,6 @@ function formatDate(date) {
   return `${year}-${month}-${day}`;
 }
 
-// function formatDate(date) {
-//   if (!date) return null;
-//   const d = new Date(date);
-//   const day = String(d.getDate()).padStart(2, "0");
-//   const month = String(d.getMonth() + 1).padStart(2, "0");
-//   const year = d.getFullYear();
-//   return `${day}-${month}-${year}`;
-// }
 
 function calculateDays(startDate, endDate) {
   if (!startDate) return 1;
