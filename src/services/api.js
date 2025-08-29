adeb8ae787e2a1157186a6de7419d465dce15d60
nazwaclarissa <reinne785@gmail.com>
2025-08-29 10:56:19 +0700

    refactor:fiks bug

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sv-varied-means-vacuum.trycloudflare.com/",
+  baseURL: "https://sterling-gst-strengths-almost.trycloudflare.com/",
   timeout: 60000,
   // headers: {
   //   "ngrok-skip-browser-warning": "true",
